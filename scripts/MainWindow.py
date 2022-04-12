fb3002d7e8d1b0bfa7e182219e37a55ea97ec657
jgre579 <jgre579@aucklanduni.ac.nz>
2022-04-12 22:25:56 +1200

    Refactor DVView for clarity

--- scripts/MainWindow.py
+++ scripts/MainWindow.py
@@ -3,7 +3,6 @@ from scripts.DVView import DVView
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.buildUI()
@@ -19,12 +18,12 @@ class MainWindow(QMainWindow):
 
         centralWidget = QWidget(self)
         self.setCentralWidget(centralWidget)
-        self.setWindowTitle('My First Application')
+        self.setWindowTitle("My First Application")
         self.move(300, 300)
         self.resize(650, 550)
         mainLayout = QVBoxLayout()
-        BaseUi = QWidget(self)
-        Ui = DVView(self)
+        BaseUi = QWidget()
+        Ui = DVView()
 
         self.stack = QStackedWidget(self)
         self.stack.addWidget(BaseUi)
@@ -36,26 +35,26 @@ class MainWindow(QMainWindow):
 
         self.stack.setCurrentIndex(1)
 
-        exitAction = QAction('Exit', self)
-        newModelAction = QAction('New Model', self)
+        exitAction = QAction("Exit", self)
+        newModelAction = QAction("New Model", self)
 
-        windowDV = QAction('Dataset Viewer', self)
-        windowBlank = QAction('Blank', self)
+        windowDV = QAction("Dataset Viewer", self)
+        windowBlank = QAction("Blank", self)
 
         windowDV.triggered.connect(lambda: self.display(1))
         windowBlank.triggered.connect(lambda: self.display(0))
 
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
+        exitAction.setShortcut("Ctrl+Q")
+        exitAction.setStatusTip("Exit application")
         exitAction.triggered.connect(qApp.quit)
 
         self.statusBar()
 
         menubar = self.menuBar()
-        menuFile = menubar.addMenu('&File')
-        menuWindow = menubar.addMenu('&Window')
-        menuOptions = menubar.addMenu('&Options')
-        menuHelp = menubar.addMenu('Help')
+        menuFile = menubar.addMenu("&File")
+        menuWindow = menubar.addMenu("&Window")
+        menuOptions = menubar.addMenu("&Options")
+        menuHelp = menubar.addMenu("Help")
         menuFile.addAction(newModelAction)
         menuFile.addAction(exitAction)
 
