d43d17678ee49ca10cd99cb6d9689fbf0ab8a5f8
jgre579 <jgre579@aucklanduni.ac.nz>
2022-04-13 16:16:36 +1200

    Add displaying of images

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import sys
 from PyQt5.QtWidgets import *
 
 from PyQt5 import QtWidgets, QtCore
+from scripts.DVView import DVView
+from scripts.MainController import MainController
 from scripts.MainWindow import MainWindow
 from scripts.Model import Model
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
 
     model = Model()
+    controller = MainController(model)
     model.load_combined_dataset()
+
+    base_ui = QWidget()
+    dv_view_ui = DVView(model)
+
+    window = MainWindow(
+        base_ui,
+        dv_view_ui,
+    )
+    window.show()
 
     sys.exit(app.exec())
